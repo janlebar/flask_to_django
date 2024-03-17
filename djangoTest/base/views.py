1fadc7418c0af1121853af194146f4b6da0e693e
JAN <janstefanlebar@gmail.com>
2024-03-17 14:43:15 +0100

    update added templates

--- djangoTest/base/views.py
+++ djangoTest/base/views.py
@@ -1,13 +1,29 @@
-# from django.shortcuts import render
-# from django.contrib import admin
-# from django.urls import path
-# from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from .forms import VINForm
+import requests
 
-# # Create your views here.
-# def home(request):
-#     return HttpResponse ('Home page')
+def generate_vin(request):
+    if request.method == 'POST':
+        form = VINForm(request.POST)
+        if form.is_valid():
+            vin_instance = form.save(commit=False)
+            vin_instance.serial_number = get_serial_number_from_server(vin_instance)
+            vin_instance.save()
+            return redirect('vin_list')
+    else:
+        form = VINForm()
+    return render(request, 'vin_generator_app/generate_vin.html', {'form': form})
 
-# urlpatterns = [
-#     path('admin/', admin.site.urls),
-#     path ('', home),
-# ]
+def get_serial_number_from_server(vin_instance):
+    # Simulate sending a POST request to the server to get serial number
+    response = requests.post('http://webtest.pekauto.com', json={
+        'version': vin_instance.version,
+        'equipment_code': vin_instance.equipment_code,
+        'year_of_issue': vin_instance.year_of_issue,
+        'place_of_production': vin_instance.place_of_production,
+    })
+    return response.text
+
+def vin_list(request):
+    vins = VIN.objects.all()
+    return render(request, 'vin_generator_app/vin_list.html', {'vins': vins})
